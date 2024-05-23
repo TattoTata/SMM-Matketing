96db311bc74af6a604165a620c47b8c6a31ce746
TattoTata <tatyana363smi@gmail.com>
2024-05-23 13:16:35 +0300

    Add files via upload

---
+++ MyWebsite/js/main.js
@@ -0,0 +1,23 @@
+
+function clickBlock1() {
+    const button = document.getElementById("bl-1");
+    button.style.backgroundColor = "black";
+}
+
+function clickBlock2() {
+    const button = document.getElementById("bl-2");
+    button.style.backgroundColor = "black";
+}
+
+function clickBlock3() {
+    const button = document.getElementById("bl-3");
+    button.style.backgroundColor = "black";
+    
+}
+
+function clickBlock4() {
+    const button = document.getElementById("bl-4");
+    button.style.backgroundColor = "black";
+}
+
+
